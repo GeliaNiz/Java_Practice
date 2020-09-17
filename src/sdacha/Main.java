cc455ef56683c299d3282c12f1c280494eb5178b
GelNiz <angelinanizam@gmail.com>
2020-09-17 19:40:02 +0500

    1

---
+++ src/sdacha/Main.java
@@ -0,0 +1,23 @@
+package sdacha;
+import java.util.Scanner;
+public class Main {
+
+        public static int k=0;
+        static void solve(int n) {
+            for(int i=n/5;i>=0;i--){
+                for(int j=(n-i*5)/3;j>=0;j--){
+                    for(int z=n-i*5-j*3;z>=0;z--){
+                        if(i*5+j*3+z==n)k++;
+                    }
+                }
+            }
+        }
+        public  static void main(String[]args){
+            Scanner sc=new Scanner(System.in);
+            int N;
+            N=sc.nextInt();
+            solve(N);
+            System.out.println(k);
+        }
+    }
+
