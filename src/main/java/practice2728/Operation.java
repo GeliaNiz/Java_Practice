875b498c7c6ad0fae134623414fea9bb49c2f9a6
GelNiz <angelinanizam@gmail.com>
2020-12-12 17:28:17 +0500

    2728

---
+++ src/main/java/practice2728/Operation.java
@@ -0,0 +1,9 @@
+package practice2728;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Operation {
+
+}
